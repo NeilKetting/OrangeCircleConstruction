4ea690e04b195c0f282392f6cb36c023aafc5a67
Neil <neil@origize63.co.za>
2026-01-06 21:11:59 +0200

    Add nullable UserId to Notifications table
    Added a new nullable UserId (Guid) column to the Notifications table via EF migration. Updated migration files and AppDbContextModelSnapshot to reflect the schema change. This allows associating notifications with specific users.

--- OCC.API/Migrations/20260106191045_fix_schema_mismatch.Designer.cs
+++ OCC.API/Migrations/20260106191045_fix_schema_mismatch.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OCC.API.Data;
 
@@ -11,9 +12,11 @@ using OCC.API.Data;
 namespace OCC.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260106191045_fix_schema_mismatch")]
+    partial class fix_schema_mismatch
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
